74dcb88264c40abc860b43ca6665a8c5f8889eee
Chris Mills <chrsmlls333@gmail.com>
2022-12-03 05:02:14 -0600

    Adjust binded folders, and ignore contents

--- gallery-dl.sh
+++ gallery-dl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker run -it --rm \
--v $(pwd)/output:/output:rw \
---mount type=tmpfs,destination=/.downloads \
+-v "/$(pwd)/output":/output:rw \
+-v "/$(pwd)/config":/config:rw \
 chrisemills/gallerydl "${1}";
