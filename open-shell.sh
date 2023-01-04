5ada29a84c82be4fe84b8e54041a452cbb0ef2c8
Chris Mills <chrsmlls333@gmail.com>
2023-01-03 21:14:36 -0600

    Rewrite open-shell script to include flags, help guidance, and debug behaviour

--- open-shell.sh
+++ open-shell.sh
@@ -1,28 +1,78 @@
 #!/usr/bin/env bash
 
-cname="gallerydl"
+# VARIABLES ########################################################
 
-run () {
+docker_image="chrisemills/gallerydl:dev"
+container_name="gallerydl"
+debug_args="--entrypoint //bin/sh"
+extra_args=''
+
+# FUNCTIONS ########################################################
+
+docker_run () {
+  echo "  Use 'exit' in the container shell to escape.
+  "
   docker run -it --rm \
-  --name $cname \
+  --name $container_name \
   -e 'UMASK'='000' \
   -e 'PUID'='99' \
   -e 'PGID'='100' \
   -v "/$(pwd)/output":/output:rw \
   -v "/$(pwd)/config":/config:rw \
-  chrisemills/gallerydl:dev
+  $extra_args $docker_image
 }
 
-if [ $(docker ps -q -f name=$cname) ]
-then
-  echo "$cname is already running... let's take a peek!"
-  docker attach $cname
-elif [ $(docker ps -aq -f status=exited -f name=$cname) ]
-then
-  echo "$cname is stopped... let's start over!"
-  docker rm $cname
-  run
-else
-  echo "$cname doesn't exist... let's fix that!"
-  run
+docker_remove () {
+  if [ $(docker ps -q -f name=$container_name) ]; then
+    docker rm $container_name
+  fi
+}
+
+docker_autocheck () {
+  if [ $(docker ps -q -f name=$container_name) ]
+  then
+    echo "Auto: $container_name is already running... let's take a peek!"
+    docker attach $container_name
+  elif [ $(docker ps -aq -f status=exited -f name=$container_name) ]
+  then
+    echo "Auto: $container_name is stopped... let's start over!"
+    docker rm $container_name
+    docker_run
+  else
+    echo "Auto: $container_name doesn't exist... let's fix that!"
+    docker_run
+  fi
+}
+
+usage() { 
+  echo "$0 usage:" && grep " .)\ #" $0; exit 0; 
+}
+
+# MAIN ##############################################################
+
+if [ $# -eq 0 ]; then
+  usage; 
+  exit 1;
 fi
+
+while getopts 'had' flag; do
+  case "${flag}" in
+    a) # Auto operation, check docker container status and run/attach.
+      docker_autocheck
+      ;;
+    d) # Use the debug Entrypoint and run a new container.
+      extra_args=$debug_args
+      docker_remove
+      docker_run
+      ;;
+    h | *) # Display help.
+      usage; exit 1;
+      ;;
+    :) 
+      echo "argument missing for $flag"; exit 1;
+      ;;
+    \?) 
+      echo "Something is wrong"; exit 1;
+      ;;
+  esac
+done
