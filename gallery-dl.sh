832b312ce246e84465cec1091e65cce06eef898e
Chris Mills <chrsmlls333@gmail.com>
2023-01-03 21:54:27 -0600

    Tweak gallery-dl script

--- gallery-dl.sh
+++ gallery-dl.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+docker_image="chrisemills/gallerydl:latest"
+container_name="gallerydl"
+
 docker run -it --rm \
+--name $container_name \
 -e 'UMASK'='000' \
 -e 'PUID'='99' \
 -e 'PGID'='100' \
 -v "/$(pwd)/output":/output:rw \
 -v "/$(pwd)/config":/config:rw \
-chrisemills/gallerydl "$@";
+$docker_image gallery-dl "$@";
